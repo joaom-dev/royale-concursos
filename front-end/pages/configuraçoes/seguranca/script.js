4d08b464f6ec59fad6cb01ea637f5483b5360ea7
Joao Marcos <marcoscd250@gmail.com>
2026-04-25 22:18:04 -0300

    tela de configuracoes de privacidade e seguranca pronta

--- front-end/pages/configuraçoes/seguranca/script.js
+++ front-end/pages/configuraçoes/seguranca/script.js
@@ -1,22 +1,4 @@
-//menu lateral
-const botao = document.querySelector('.botao-menu')
-const menuLateral = document.querySelector('.menu-lateral')
-const conteudo = document.querySelector('body')
-const background = document.querySelector('.background')
 
-botao.addEventListener('click', () => {
-    menuLateral.classList.toggle('ativo')
-    conteudo.classList.toggle('ativo')
-    background.classList.toggle('ativo')
-    botao.classList.toggle('ativo')
-})
-
-background.addEventListener('click', () => {
-    menuLateral.classList.remove('ativo')
-    conteudo.classList.remove('ativo')
-    background.classList.remove('ativo')
-    botao.classList.remove('ativo')
-})
 
 // toggle 2FA
 const toggle2FA = document.getElementById("toggle-2fa")
