37bbc59094aea1050e8a00852a499fc3699e9213
GustavoH <gustavohenrick.27.8.2007@gmail.com>
2026-06-16 09:22:21 -0300

    sistema de alteração de dados e de anuncios

--- back-end/src/main/java/com/royaleconcursos/security/SecurityConfig.java
+++ back-end/src/main/java/com/royaleconcursos/security/SecurityConfig.java
@@ -13,6 +13,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.http.HttpMethod;
 
 @Configuration
 @EnableWebSecurity
@@ -31,6 +34,11 @@ public class SecurityConfig {
                 .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
             )
             .authorizeHttpRequests(auth -> auth
+
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+
+                .requestMatchers(HttpMethod.GET,"/anuncios/**").permitAll()
+
                 // Rotas públicas
                 .requestMatchers(
                     "/auth/login",
@@ -38,9 +46,11 @@ public class SecurityConfig {
                     "/login**",
                     "/error**",
                     "/oauth2/**",
-                    "/api/concursos/**"   // leitura pública dos concursos
+                    "/api/concursos/**",   // leitura pública dos concursos
+                    "/fotos/**"
                 ).permitAll()
                 .anyRequest().authenticated()
+
             )
             // Login com Google
             .oauth2Login(oauth2 -> oauth2
@@ -60,5 +70,17 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public WebMvcConfigurer resourceHandler() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/fotos/**")
+                .addResourceLocations("file:upload/fotos/");
+            }
+        };
     }
 }
